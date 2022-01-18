a2b63c262512e0c9438d9e69760410be2e64c815
njguld95038043 <t.tamura6060@gmail.com>
2022-01-18 12:38:26 +0000

    [Update]2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -40,6 +41,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body, :user_id)
   end
 end
