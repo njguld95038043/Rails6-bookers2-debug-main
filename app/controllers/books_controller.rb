3a79f74e2ec724fe84fa48f754c4631a59d3c0ef
njguld95038043 <t.tamura6060@gmail.com>
2022-01-19 01:07:27 +0000

    [Update]2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,11 +11,12 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
-      @books = Book.all
-      render 'index'
+    @books = Book.all
+    render 'index'
     end
   end
 
@@ -28,7 +29,7 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render "edit"
+    render "edit"
     end
   end
 
@@ -41,6 +42,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :user_id)
+    params.require(:book).permit(:title, :body)
   end
 end
